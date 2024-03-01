ab8821c7f0c1e81447a3622bc27a916767ace0b4
@giangnntse173469 <giangnntse173469@fpt.edu.vn>
2024-03-01 13:55:18 +0700

    roleconst update

--- src/Application/Shared/Constant/Role.cs
+++ src/Application/Shared/Constant/Role.cs
@@ -9,9 +9,12 @@ namespace net03_group02.src.Application.Shared.Constant
     {
         public const string ADMIN = "ADMIN";
         public const int ADMIN_ID = 1;
+        public const string MEMBER = "MEMBER";
+        public const int MEMBER_ID = 6;
         public Dictionary<string, int> RoleId = new()
         {
             { ADMIN, ADMIN_ID },
+            { MEMBER, MEMBER_ID},
         };
         public static int GetRoleId(string roleName)
         {
